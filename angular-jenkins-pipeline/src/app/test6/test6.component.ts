225fe35d16c6a2d58603085c12802de5dc68890c
Ashish <ashish.sharma@lyxellabs.com>
2023-01-09 12:59:31 +0100

    added 7

---
+++ angular-jenkins-pipeline/src/app/test6/test6.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-test6',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './test6.component.html',
+  styleUrls: ['./test6.component.css']
+})
+export class Test6Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
